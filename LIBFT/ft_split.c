39aad23ca6563b52e40af5655d0aee83df662a53
ulol <jeffk0408@naver.com>
2022-05-21 01:39:25 +0900

    DEL : duplicated func : ft_strndup from ft_split

--- LIBFT/ft_split.c
+++ LIBFT/ft_split.c
@@ -6,14 +6,14 @@
 /*   By: dkim2 <dkim2@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 23:40:46 by dkim2             #+#    #+#             */
-/*   Updated: 2021/11/29 22:52:15 by dkim2            ###   ########.fr       */
+/*   Updated: 2022/05/21 01:39:01 by dkim2            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-int	count_delimeter(char const *s, char c)
+static int	count_delimeter(char const *s, char c)
 {
 	int	i;
 	int	is_delimeter;
@@ -35,28 +35,6 @@ int	count_delimeter(char const *s, char c)
 		i++;
 	}
 	return (count);
-}
-
-static char	*ft_strndup(const char *s1, size_t n)
-{
-	size_t	i;
-	size_t	size;
-	char	*copy;
-
-	size = ft_strlen(s1);
-	if (size < n)
-		n = size;
-	copy = malloc(sizeof(char) * (n + 1));
-	if (!copy)
-		return (NULL);
-	i = 0;
-	while (i < n)
-	{
-		copy[i] = s1[i];
-		i++;
-	}
-	copy[i] = 0;
-	return (copy);
 }
 
 char	**ft_split(char const *s, char c)
