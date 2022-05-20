143970f0049c1e721f6a4c2c2342494a1b64d450
YH-Yoo <yehuen981@naver.com>
2022-05-20 21:04:44 +0900

    AddCommet_env

--- SRC/builtins/env/env.c
+++ SRC/builtins/env/env.c
@@ -1,19 +1,6 @@
-# include <stdio.h>
-# include <stdlib.h>
-
-typedef struct s_envnode
-{
-	char		*key;
-	char		*value;
-	struct s_envnode	*nextnode;	// 다음 인덱스의 환경변수
-}   t_envnode;
-
-typedef struct s_env
-{
-	int			element;	// 환경변수 개수
-	t_envnode	*phead;	// 헤드노드
-	t_envnode	*ptail; // 꼬리노드
-}   t_env;
+#include <stdio.h>
+#include <stdlib.h>
+#include "../../../INC/minishell_datastructure.h"
 
 int	ft_strlen(char *str)
 {
@@ -30,12 +17,12 @@ int	ft_keylen(char *str)	//key 길이 측정
 	int	i;
 
 	i = 0;
-	while (str[i] != '=')
+	while (str[i] != '=')	// '=' 전까지
 		i++;
 	return (i);
 }
 
-void	cp_element(char *envp, t_env *env, t_envnode *new_node, int len, int keylen)
+void	copy_element(char *envp, t_env *env, t_envnode *new_node, int len, int keylen)
 {
 	char		*key;
 	char		*value;
@@ -44,21 +31,21 @@ void	cp_element(char *envp, t_env *env, t_envnode *new_node, int len, int keylen
 	key = malloc(sizeof(char) * (keylen + 1));
 	value = malloc(sizeof(char) * (len - keylen));
 	j = 0;
-	while(envp[j] && j < keylen)
+	while(envp[j] && j < keylen)		// key길이 만큼 copy
 	{
 		key[j] = envp[j];
 		j++;
 	}
 	key[j] = '\0';
 	j = 0;
-	while (envp[j + keylen] && j < (len - keylen - 1))
+	while (envp[j + keylen] && j < (len - keylen - 1))	// 전체 길이 - key길이 - 1(해주는 이유는 '=' 때문) value copy
 	{
 		value[j] = envp[j + keylen + 1];
 		j++;
 	}
 	value[j] = '\0';
-	new_node->key = key;
-	new_node->value = value;
+	new_node->key = key;		// key 노드에 저장
+	new_node->value = value;	// value 노드에 저장
 }
 
 void	add_node(char *envp, t_env *env, int i)
@@ -71,17 +58,17 @@ void	add_node(char *envp, t_env *env, int i)
 	keylen = ft_keylen(envp);
 	new_node = malloc(sizeof(t_envnode));
 	new_node->nextnode = NULL;
-	if (i == 0)
+	if (i == 0)				// 첫 노드 생성일 때
 	{
-		env->phead = new_node;
-		env->ptail = new_node;
+		env->phead = new_node;	// 헤더 노드 -> new_node;
+		env->ptail = new_node;	// 꼬리 노드 -> new_node;
 	}
-	else
+	else					//	두번째 노드 부터는 여기
 	{
-		env->ptail->nextnode = new_node;
-		env->ptail = new_node;
+		env->ptail->nextnode = new_node;	// 꼬리 노드의 다음노드 -> new_node
+		env->ptail = new_node;				// 꼬리 노드 -> new_node
 	}
-	cp_element(envp, env, new_node, len, keylen);
+	copy_element(envp, env, new_node, len, keylen);	// key ,value를 노드에 저장
 }
 
 void	pisplay_env(t_env *env)
@@ -112,15 +99,13 @@ void	do_env(int argc, char **argv, char **envp)
 
 	while (envp[i])		// 환경변수를 env 리스트에 넣는 함수
 	{
-		add_node(envp[i], env, i);
+		add_node(envp[i], env, i);	// 노드를 추가
 		i++;
 	}
-	printf("%d\n\n", env->element);
 	pisplay_env(env);
 }
 
-int main(int argc, char **argv, char **envp)
-{
-	do_env(argc, argv, envp);
-}
-
+// int main(int argc, char **argv, char **envp)
+// {
+// 	do_env(argc, argv, envp);
+// }
