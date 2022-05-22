398b2ee6716c5aca0628725c4914b60d48814a8a
ulol <jeffk0408@naver.com>
2022-05-23 04:09:27 +0900

    conflict fix

--- SRC/TOKENIZE/token_node.c
+++ SRC/TOKENIZE/token_node.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -7,12 +6,10 @@
 /*   By: dkim2 <dkim2@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/22 18:32:41 by dkim2             #+#    #+#             */
-/*   Updated: 2022/05/22 18:38:20 by dkim2            ###   ########.fr       */
+/*   Updated: 2022/05/23 04:09:04 by dkim2            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-=======
->>>>>>> 482b848a2cb3ea3acef8068f9c541b6ca1cd5229
 #include "../../INC/minishell.h"
 #include <stdlib.h>
 
@@ -20,11 +17,7 @@ t_token	*create_token(char	*word)
 {
 	t_token	*new_tok;
 
-<<<<<<< HEAD
 	new_tok = ft_calloc(1, sizeof(t_token));
-=======
-	new_tok = ft_calloc(sizeof(t_token));
->>>>>>> 482b848a2cb3ea3acef8068f9c541b6ca1cd5229
 	if (new_tok == NULL)
 		return (NULL);
 	if (word != NULL)
@@ -40,7 +33,6 @@ t_token	*create_token(char	*word)
 	return (new_tok);
 }
 
-<<<<<<< HEAD
 int	is_eof_token(t_token *tok)
 {
 	if (tok->text == NULL)
@@ -54,11 +46,3 @@ void	free_token(t_token *tok)
 		free(tok->text);
 	free (tok);
 }
-=======
-void	free_token(t_token *tok)
-{
-	if (!tok)
-		free(tok->text);
-	free (tok);
-}
->>>>>>> 482b848a2cb3ea3acef8068f9c541b6ca1cd5229
