10674d3dbc6416c43e26be61f8c4341340fea878
ulol <jeffk0408@naver.com>
2022-06-02 21:36:00 +0900

    dkim2 try read heredoc by readline.h

--- SRC/EXECUTE/inredir.c
+++ SRC/EXECUTE/inredir.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <readline/readline.h>
 
 int	check_infile(t_token_list *inredir)
 {
@@ -35,19 +36,55 @@ int	do_inredir(t_token_list *inredir)
 	return (0);
 }
 
-void	read_here_doc(t_token *curr, int *fd)
-{
-	char	buf[1024];
-	int		r;
+/*
+ * ft_cmp 대신 비슷한 기존 함수인 ft_strncmp사용
+ * ft_cmp 사용 시 EOF 일 때 종료 안 됨.
+ * read == 0 or -1인 경우 예외처리 추가 함.
+ * buf[r]이 아닌 buf[r - 1]을 하여 개행문자 삭제함.
+*/
+// void	read_here_doc(t_token *curr, int *fd)
+// {
+// 	char	buf[1024];
+// 	int		r;
 
-	while (1)
+// 	while (1)
+// 	{
+// 		write(1, "> ", 2);
+// 		r = read(0, buf, 1024);
+// 		if (r <= 0)
+// 			break ;
+// 		buf[r - 1] = 0;
+// 		if (ft_strncmp(curr->text, buf, ft_strlen(buf) + 1) == 0)
+// 			break ;
+// 		write(fd[1], buf, ft_strlen(buf));
+// 		write(fd[1], "\n", 1);
+// 	}
+// }
+
+/*
+ * 고정된 버퍼와 read사용 시 읽을 수 있는 문자열의 길이에 제한이 생김.
+ * -> readline함수로 대체 시도.
+*/
+void	read_here_doc(t_token *currtok, int *fd)
+{
+	char	*key;
+	char	*buffer;
+	int		key_len;
+
+	key = currtok->text;
+	if (key == NULL)
+		return ;
+	key_len = ft_strlen(key);
+	while (TRUE)
 	{
-		write(1, "> ", 2);
-		r = read(0, buf, 1024);
-		buf[r] = 0;
-		if (ft_cmp(curr->text, buf))
+		buffer = readline("heredoc > ");
+		if (buffer == NULL)
 			break ;
-		write(fd[1], buf, ft_strlen(buf));
+		else if (ft_strncmp(key, buffer, key_len + 1) == 0)
+			break ;
+		write(fd[1], buffer, ft_strlen(buffer));
+		write(fd[1], "\n", 1);
+		free(buffer);
 	}
 }
 
